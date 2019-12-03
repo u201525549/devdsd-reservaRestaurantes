83627292b02055fe10e631a86efa356b07741a9e
u201525549 <u201525549@upc.edu.pe>
2019-12-03 04:28:45 -0500

    mantenimiento Usuarios interface vs01
    Se habilita las interfaz de  mantenimiento de usuario con la capa dominio para el acceso a la data. indirecto.

---
+++ ServiciosWeb.Dominio/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiciosWeb.Dominio
+{
+    public class Usuario
+    {
+        public long usua_id { get; set; }
+        public string usua_email { get; set; }
+        public string usua_pass { get; set; }
+        public string usua_nomb { get; set; }
+        public string usua_dni { get; set; }
+        public string usua_direc { get; set; }
+        public Nullable<int> dist_id { get; set; }
+        public string usua_refedirec { get; set; }
+        public System.DateTime? usua_fecnac { get; set; }
+        public string usua_rutaimagen { get; set; }
+    }
+}
